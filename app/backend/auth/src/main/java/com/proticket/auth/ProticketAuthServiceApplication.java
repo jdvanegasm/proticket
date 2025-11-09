73553aad8146b1e187c19def4c9cb61ad9688895
jdvanegasm <jdvanegasm@outlook.com>
2025-11-09 18:50:00 -0500

    dev: auth backend setup finished

---
+++ app/backend/auth/src/main/java/com/proticket/auth/ProticketAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.proticket.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProticketAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProticketAuthServiceApplication.class, args);
+	}
+
+}
---
+++ app/backend/auth/src/test/java/com/proticket/auth/ProticketAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proticket.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProticketAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
